efb82f4e05df3e33b60be5c610f2d653ff36c7f0
Lyna Mazri <setherine13@gmail.com>
2024-01-16 20:03:53 +0100

    upload pdf

--- app/src/main/java/com/example/registrecommobile/SubmitRequestActivity.java
+++ app/src/main/java/com/example/registrecommobile/SubmitRequestActivity.java
@@ -1,8 +1,12 @@
 package com.example.registrecommobile;
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -21,7 +25,7 @@ public class SubmitRequestActivity extends AppCompatActivity {
             editTextDateOfBirth, editTextNationality, editTextIdNumber;
 
     private CheckBox checkBoxDeclaration;
-
+    private static final int PICK_PDF_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +42,7 @@ public class SubmitRequestActivity extends AppCompatActivity {
         editTextIdNumber = findViewById(R.id.editTextIdNumber);
 
         checkBoxDeclaration = findViewById(R.id.checkBoxDeclaration);
-
+        Button buttonChooseFile = findViewById(R.id.buttonChooseFile);
         Button buttonSubmitRequest = findViewById(R.id.buttonSubmitRequest);
         Button buttonCancelRequest = findViewById(R.id.buttonCancelRequest);
 
@@ -61,7 +65,53 @@ public class SubmitRequestActivity extends AppCompatActivity {
                 onSubmitRequest();
             }
         });
+
+        buttonChooseFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseFile();
+            }
+        });
+
     }
+
+    private void chooseFile() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+        startActivityForResult(intent, PICK_PDF_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Button buttonChooseFile = findViewById(R.id.buttonChooseFile);
+        if (requestCode == PICK_PDF_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri selectedFileUri = data.getData();
+            String fileName = getFileName(selectedFileUri);
+            Toast.makeText(this, "Selected file: " + fileName, Toast.LENGTH_SHORT).show();
+            buttonChooseFile.setEnabled(false);
+            buttonChooseFile.setText(fileName);
+        }
+    }
+
+    @SuppressLint("Range")
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            }
+        }
+        if (result == null) {
+            result = uri.getLastPathSegment();
+        }
+        return result;
+    }
+
+
 
     private void onSubmitRequest() {
 
